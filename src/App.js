9c1d6d287db3dd438639aecf870aaa4a122b0701
PRABHAT CHANDRA DWIVEDI <mr.prabhatdwivedi@gmail.com>
2023-12-19 00:48:26 +0530

    adding snap logic using framer motion
    adding snapping logic for circle-part one of two.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Circle from './Circle/Circle';
 import Grid from './Grid/Grid';
 import { motion } from "framer-motion";
 
 function App() {
+  const [gridCellHeight,setGridCellHeight] = useState(0);
+  const [gridCellWidth,setGridCellWidth] = useState(0);
+  const [screenWidth,setScreenWidth] = useState(0);
+  const [screenHeight,setScreenHeight] = useState(0);
+  const getGridCellHeight = (height)=>{
+    setGridCellHeight(height)
+  }
+  const getGridCellWidth = (width)=>{
+    setGridCellWidth(width)
+  }
   const constraintsRef = useRef(null);
+  useEffect(()=>{
+setScreenHeight(window.screen.availHeight);
+setScreenWidth(window.screen.availWidth);
+  },[])
+  console.log(screenHeight);
+  console.log(screenWidth);
   return (
     <motion.div ref={constraintsRef}>
     
-    <div className='flex justify-center items-center h-screen w-screen bg-black'>
-  <Grid/>
-  <Circle constraintsRef ={constraintsRef}/>
+    <div className='flex flex-col justify-center items-center h-screen w-screen bg-black'>
+  <Circle constraintsRef ={constraintsRef} gridCellWidth={gridCellWidth} gridCellHeight={gridCellHeight} />
+  <Grid getGridCellHeight={getGridCellHeight} getGridCellWidth={getGridCellWidth} />
+
   </div>
   </motion.div>
   );
--- src/Circle/Circle.js
+++ src/Circle/Circle.js
@@ -1,14 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 
-import React from 'react'
-import { motion } from "framer-motion";
-export default function Circle({constraintsRef}) {
-  
+export default function Circle({ constraintsRef,gridCellWidth,gridCellHeight }) {
+  const controls = useAnimation();
+  const [CellWidth,setCellWidth] = useState(0);
+  const [cellHeight,setCellHeight] = useState(0);
 
+  const handleDragEnd = (event, info) => {
+    const x = info.offset.x + info.point.x;
+    const y = info.offset.y + info.point.y;
+    console.log(info.point.x);
+    console.log(info.point.y);
+
+    const nearestX = Math.round(x / CellWidth) * CellWidth;
+    const nearestY = Math.round(y / cellHeight) * cellHeight;
+
+
+    controls.start({ x: 0, y: 0 });
+  };
+  useEffect(()=>{
+    setCellHeight(gridCellHeight);
+    setCellWidth(gridCellWidth);
+    },[gridCellWidth,gridCellHeight])
+    console.log(cellHeight)
+    console.log(CellWidth)
   return (
-    
-        <motion.div  drag dragConstraints={constraintsRef}>
-    <div class="w-32 h-32 border-white bg-black border-4 rounded-full"></div>
-    </motion.div>
-    
-  )
+    <motion.div
+      drag
+      dragConstraints={constraintsRef}
+      onDragEnd={handleDragEnd}
+      animate={controls}
+      className='h-1/6 w-1/6'
+    >
+<div className='h-3/4 w-3/4'>
+  <svg id="go" 
+    viewBox="0 0 100 100"
+    perserveAspectRatio="xMinYMid" className='h-full w-full'>
+    <circle r="48" cx="50%" cy="50%" fill="black" stroke='white' strokeWidth={4}></circle>
+  </svg>
+</div>    </motion.div>
+  );
 }
